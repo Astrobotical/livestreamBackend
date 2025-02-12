0e1807b1a7b4e06bdab98dd27b4c6154c007a385
romarioburke <romari123456789@gmail.com>
2025-02-12 17:25:23 -0500

    Api endpoint restructuring

--- routes/api.php
+++ routes/api.php
@@ -11,37 +11,37 @@ use App\Http\Controllers\api\admin\Tags;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
 //Authentication Group api/auth/
-Route::group(['prefix'=>'auth'],function (){
-    Route::post('login',[authController::class, 'userLogin']);
-    Route::post('signup',[authController::class, 'userRegistration']);
-    Route::post('validate',[authController::class, 'doesuserExists']);
-    Route::get('verify',[authController::class, 'verifyUser'])->middleware('auth:sanctum');
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [authController::class, 'userLogin']);
+    Route::post('signup', [authController::class, 'userRegistration']);
+    Route::post('validate', [authController::class, 'doesuserExists']);
+    Route::get('verify', [authController::class, 'verifyUser'])->middleware('auth:sanctum');
 });
 // Admin/Moderator only Group api/admin/
 Route::group(['prefix' => 'admin', 'middleware' => ['auth:sanctum']], function () {
     Route::get('getUsers', [adminController::class, 'getAllUsers']);
     Route::post('create-user', [adminController::class, 'createUser']);
     Route::post('schedule', [livestreamController::class, 'store']);
-    Route::get('getStreams',[livestreamController::class, 'index']);
-    Route::post('banUser',[UserController::class,'banUser']);
-        Route::get('tags', [Tags::class, 'getTags']);
-    
-        Route::get('tags/search', [Tags::class, 'searchTags']);
+    Route::get('getStreams', [livestreamController::class, 'index']);
+    Route::post('banUser', [UserController::class, 'banUser']);
+    Route::get('tags', [Tags::class, 'getTags']);
 
-        Route::post('tags', [Tags::class, 'addTag']);
-        
-        Route::post('users/{userId}/tags', [Tags::class, 'addTagsToUser']);
-        Route::put('updateUser/{userId}', [adminController::class, 'updateUser']);
-        // In routes/api.php
-        Route::post('startStream/{id}', [adminController::class, 'startStream']);
-        Route::post('endStream/{id}', [adminController::class, 'endStream']);
-    });
+    Route::get('tags/search', [Tags::class, 'searchTags']);
+
+    Route::post('tags', [Tags::class, 'addTag']);
+
+    Route::post('users/{userId}/tags', [Tags::class, 'addTagsToUser']);
+    Route::put('updateUser/{userId}', [adminController::class, 'updateUser']);
+    // In routes/api.php
+    Route::post('startStream/{id}', [adminController::class, 'startStream']);
+    Route::post('endStream/{id}', [adminController::class, 'endStream']);
+});
 //General Routes but the user has to be authenticated to use them
-Route::get('getNextStream',[livestreamController::class, 'getNextStream']);
+Route::get('getNextStream', [livestreamController::class, 'getNextStream']);
 Route::group(['prefix' => 'user', 'middleware' => ['auth:sanctum']], function () {
-Route::get('get',[UserController::class, 'getUserDetails']);
-Route::post('checkpassword',[UserController::class,'checkUserPassword']);
-Route::put('update',[UserController::class,'updateUser']);
-}); 
-
+    Route::get('get', [UserController::class, 'getUserDetails']);
+    Route::post('checkpassword', [UserController::class, 'checkUserPassword']);
+    Route::put('update', [UserController::class, 'updateUser']);
+});
